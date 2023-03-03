a534d64b8490e466dab212ddfadf5ae0195b24a1
Lucas <lucasprc93@gmail.com>
2023-03-02 21:58:36 -0800

    Initial Commit

---
+++ App-Framework/App_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_FrameworkApp.swift
+//  App-Framework
+//
+//  Created by Lucas on 2023-03-02.
+//
+
+import SwiftUI
+
+@main
+struct App_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
